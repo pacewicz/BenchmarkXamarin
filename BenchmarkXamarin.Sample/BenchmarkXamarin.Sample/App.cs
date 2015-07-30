596390b7a124f23ee1bd60afc966f91612ff8a70
Kugushev Aleksandr <kugushew@gmail.com>
2015-07-30 17:51:20 +0300

    Add full version

---
+++ BenchmarkXamarin.Sample/BenchmarkXamarin.Sample/App.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace BenchmarkXamarin.Sample
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var log = new Label();
+
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Start,
+                    Children = {
+                        new Button {
+                            Text = "Start benchmark",
+                            Command = new Command(OnStartBenchmarkClick)
+                        },
+                        log
+                    }
+                }
+            };
+
+            BenchmarkManager manager = BenchmarkManager.Current;
+            manager.Log += text => log.Text += Environment.NewLine + text;                       
+            manager.Register(GetType().GetTypeInfo().Assembly);
+        }
+
+        private void OnStartBenchmarkClick(object obj)
+        {
+            BenchmarkManager.Current.Start();
+        }
+    }
+}
---
+++ BenchmarkXamarin.Sample/BenchmarkXamarin.Sample/BenchmarksString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkXamarin.Sample
+{
+    class BenchmarksString
+    {
+        [Benchmark]
+        public void Substring()
+        {
+            "musculus cremaster".Substring(8, 9);
+        }
+
+        [Benchmark]
+        public void Split()
+        {
+            "  lingua latina non  verpa canina  ".Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        [Benchmark]
+        public void Trim()
+        {
+            "    lorem ipsum      ".Trim();
+        }
+
+        [Benchmark]
+        public void StartWith()
+        {
+            "hello world, ребята".StartsWith("h");
+        }
+    }
+}
